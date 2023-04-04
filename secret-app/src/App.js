987ea960653dbc9401719b8a45c4677731218dd5
João Vitor Melo Kremer <joaokremer.dev@gmail.com>
2023-04-03 21:53:16 -0300

    categorias e palavras criadas

--- secret-app/src/App.js
+++ secret-app/src/App.js
@@ -22,9 +22,36 @@ function App() {
   const [gameStage, setGameStage] = useState(stages[0].name);
   const [words] = useState(wordsList);
 
+  const [pickedWord, setPickedWord] = useState("");
+  const [pickedCategory, setPickedCategory] = useState("");
+  const [letters, setLetters] = useState([]);
+
+  const pickWordAndCategory = () => {
+    //Pick a random category
+    const categories = Object.keys(words)
+    const category = categories[Math.floor(Math.random() * Object.keys(categories).length)]
+    //Pick a random word
+    const word = words[category][Math.floor(Math.random() * words[category].length)]
+
+    return {word, category};
+  }
+
 //Starts the secret word game
   const startGame = () => {
+    // pick word and pick category
+    const { word, category } = pickWordAndCategory();
+    
+    //Create an array of letters
+    let wordLetters = word.split("")
+    wordLetters = wordLetters.map((letter) => letter.toLowerCase())
+    
+    console.log(wordLetters)
+    console.log(word, category)
+
     setGameStage(stages[1].name)
+    setPickedCategory(category)
+    setPickedWord(word)
+    setLetters(wordLetters)
   }
 
 // Process the letter input
@@ -33,7 +60,7 @@ function App() {
   }
 // restarts the game 
   const retry = () => {
-    setGameStage(stages[0].name);
+    setGameStage(stages[0].name); 
   }
 
   return (
